f7cd5ac6a42240eecc7c370416cf152fe0a205d5
jackbartley8 <43116807+jackbartley8@users.noreply.github.com>
2020-12-20 10:24:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,5 +25,6 @@ setup(
     zip_safe=False,
     install_requires=[
     "requests>=2.23.0"
-    ]
+    ],
+    dependency_links=['http://github.com/jackbartley8/company_identification/master#egg=package-0.1']
 )
