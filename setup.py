7836ae6c14b27c2aa82924f4be917b83a3faad97
jackbartley8 <43116807+jackbartley8@users.noreply.github.com>
2020-12-19 23:20:57 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import os
+# import sys
+import textwrap
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='company_identification',
+    version='0.1',
+    author='Jack Bartley',
+    packages=[
+        'company_identification'],
+    url='https://github.com/jackbartley8/company_identification',
+    description=(
+        "jacks use only"
+        " yo"
+    ),
+    long_description=textwrap.dedent(
+        open(os.path.join(ROOT, 'README.rst')).read()),
+    include_package_data=True,
+    zip_safe=False,
+    #install_requires=[
+
+    #],
